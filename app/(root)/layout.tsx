55d1ef8b85306b73060abfab55553478893ac27a
unknown <jatinvashisht11@gmail.com>
2023-11-12 12:47:09 +0530

    First Commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,41 @@
+import { Inter } from 'next/font/google'
+import '../globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Threads',
+  description: `Say more with Threads — Instagram's new text app.`
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <link rel="icon" href='./logo.png' />
+        <body className={inter.className}>
+          <Topbar />
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
